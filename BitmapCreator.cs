38f3d7ab5e8c059a918a05904884e51512dad0ad
AdventureT <le@nepelius.at>
2023-06-02 16:42:34 +0200

    Removed converting to ARGB instead use Indexed palette format in bmp

---
+++ BitmapCreator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class BitmapCreator
+{
+    public static Bitmap CreateBitmap(byte[] compressedImageData, Color[] palette, int width, int height)
+    {
+        if (palette.Length != 256)
+        {
+            throw new ArgumentException("Invalid palette length");
+        }
+
+        byte[] imageData = DecompressRLE(compressedImageData, compressedImageData.Length, width * height);
+
+        Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+
+        ColorPalette colorPalette = bitmap.Palette;
+        for (int i = 0; i < palette.Length; i++)
+        {
+            colorPalette.Entries[i] = palette[i];
+        }
+        bitmap.Palette = colorPalette;
+
+        BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height),
+                                                ImageLockMode.WriteOnly,
+                                                PixelFormat.Format8bppIndexed);
+
+        int stride = bitmapData.Stride;
+        IntPtr scan0 = bitmapData.Scan0;
+
+        unsafe
+        {
+            fixed (byte* imageDataPtr = imageData)
+            {
+                byte* imagePtr = imageDataPtr;
+                byte* bitmapPtr = (byte*)scan0;
+
+                for (int y = 0; y < height; y++)
+                {
+                    Buffer.MemoryCopy(imagePtr, bitmapPtr, width, width);
+                    imagePtr += width;
+                    bitmapPtr += stride;
+                }
+            }
+        }
+
+        bitmap.UnlockBits(bitmapData);
+
+        return bitmap;
+    }
+
+    // Decompress RLE Buffer
+    static byte[] DecompressRLE(byte[] data, int compressedSize, int decompressedSize)
+    {
+        var inOff = 0;
+        var outOff = 0;
+        var decompressedBuffer = new byte[decompressedSize];
+        while (inOff < compressedSize)
+        {
+            var controlByte = data[inOff++];
+            int count;
+
+            if (controlByte >= 128)
+            {
+                count = 256 - controlByte;
+
+                // Copy Bytes no RLE needed
+                Buffer.BlockCopy(data, inOff, decompressedBuffer, outOff, count);
+
+                inOff += count;
+            }
+            else
+            {
+                count = controlByte;
+                byte repeatByte = data[inOff++];
+                // Repeat Bytes with count: RLE
+                for (int i = 0; i < count; i++)
+                {
+                    decompressedBuffer[outOff + i] = repeatByte;
+                }
+            }
+
+            outOff += count;
+        }
+
+        return decompressedBuffer;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,84 +19,15 @@ for (int i = 0; i < 256; i++) // Iterate through palette 256 color
 br.BaseStream.Seek(0x80C, SeekOrigin.Begin);
 var imgHeader = new ImageHeader(br.ReadInt32(), br.ReadInt32(), br.ReadUInt32(), br.ReadUInt32(), br.ReadUInt32());
 
-List<byte> compressedBuffer = new();
-
 for (int i = 0; i < imgHeader.countOfSubImages; i++)
 {
     var subImgHeader = new SubImageHeader(br.ReadInt32(), br.ReadInt32(), br.ReadUInt32(), br.ReadUInt32(), br.ReadUInt32(), br.ReadUInt32(), br.ReadInt32(), br.ReadUInt32());
 
-    compressedBuffer.AddRange(br.ReadBytes(subImgHeader.sizeOfSubImage));
+    byte[] compressedBuffer = br.ReadBytes(subImgHeader.sizeOfSubImage);
 
-    var decompBuffer = DecompressRLE(compressedBuffer.ToArray(), subImgHeader.sizeOfSubImage, (int)(subImgHeader.width * subImgHeader.height));
+    var bmp = BitmapCreator.CreateBitmap(compressedBuffer, palette, subImgHeader.width, subImgHeader.height);
 
-    compressedBuffer.Clear();
-
-    var rawData = ToARGB32(subImgHeader.width, subImgHeader.height, decompBuffer, palette);
-
-    var bmp = new Bitmap(subImgHeader.width, subImgHeader.height, PixelFormat.Format32bppArgb);
-
-    // Copy 32bppArgb data to BitmapData
-    BitmapData sourceData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
-    Marshal.Copy(rawData, 0, sourceData.Scan0, rawData.Length);
-    bmp.UnlockBits(sourceData);
-
-    // Save image
     bmp.Save(args[0] + "_" + (i + 1) + ".bmp", ImageFormat.Bmp);
-}
-
-// Convert Format8bppIndexed to Format32bppArgb
-static int[] ToARGB32(int width, int height, byte[] imgData, Color[] palette)
-{
-    var rgb = new int[width * height];
-    
-    for (int y = 0; y < height; y++)
-    {
-        for (int x = 0; x < width; x++)
-        {
-            var val = imgData[x + (y * width)];
-            var col = palette[val];
-            rgb[x + (y * width)] = col.ToArgb();
-        }
-    }
-    return rgb;
-}
-
-// Decompress RLE Buffer
-static byte[] DecompressRLE(byte[] data, int compressedSize, int decompressedSize)
-{
-    var inOff = 0;
-    var outOff = 0;
-    var decompressedBuffer = new byte[decompressedSize];
-    while (inOff < compressedSize)
-    {
-        var controlByte = data[inOff++];
-        int count;
-
-        if (controlByte >= 128)
-        {
-            count = 256 - controlByte;
-
-            // Copy Bytes no RLE needed
-            Buffer.BlockCopy(data, inOff, decompressedBuffer, outOff, count);
-
-            inOff += count;
-        }
-        else
-        {
-            count = controlByte;
-            var repeatByte = data[inOff++];
-            // Repeat Bytes with count: RLE
-            for (int i = 0; i < count; i++)
-            {
-                decompressedBuffer[outOff + i] = repeatByte;
-            }
-        }
-
-        outOff += count;
-    }
-
-    return decompressedBuffer;
-
 }
 
 record ImageHeader(int width, int height, uint bpp, uint isCompressed, uint countOfSubImages);
